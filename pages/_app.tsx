e11d6d3e70e1bf361f7616da8d2c42881adb6381
VanAubrey <vanaubreyd19@gmail.com>
2025-07-30 16:34:38 +0800

    feat: add nextauth integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ 
+  Component, 
+  pageProps: { session, ...pageProps } 
+}: AppProps) {
+  // Use type assertion to tell TypeScript this is safe
+  const SafeComponent = Component as unknown as React.ComponentType<any>;
+  
+  return (
+    <SessionProvider session={session}>
+      <SafeComponent {...pageProps} />
+    </SessionProvider>
+  );
 }
